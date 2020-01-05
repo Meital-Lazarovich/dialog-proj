7de6e1667adb7521be8fdcb170fa5920a0a6b4ed
Meital Lazarovich <lazarovich.meital@gmail.com>
2020-01-05 16:50:39 +0200

    creating basic component

---
+++ src/components/DialogBox.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom'
+
+const DialogBox = (props) => {
+    const [isVisable, setIsVisable] = useState(true);
+
+    const handleKeydown = (ev) => {
+        if (ev.key === 'Escape') {
+            setIsVisable(false)
+        };
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeydown);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeydown);
+        };
+    });
+
+    return ReactDOM.createPortal(
+        <>
+            <div className="visual-overlay"></div>
+            <dialog style={{ display: isVisable ? 'block': 'none'}}>
+                <div role="document">
+                    {props.children}
+                </div>
+            </dialog>
+        </>,
+        document.getElementById('dialogPlaceholder')
+    )
+}
+
+export default DialogBox;
---
+++ src/views/MainContent.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import DialogBox from '../components/DialogBox'
+
+class MainContent extends Component {
+    state = {
+        isModalOpen: false
+    }
+
+    openDialog = () => {
+        this.setState({ isModalOpen: true })
+    }
+
+    render() {
+        const { isModalOpen } = this.state
+        return (
+            < section className="main-content" >
+                <h1>Welcome to the app</h1>
+                <button onClick={this.openDialog}>Open dialog</button>
+                {isModalOpen && 
+                <DialogBox>
+                    <h2>I am inside the slot</h2>
+                </DialogBox>}
+            </section >
+        )
+    }
+}
+
+export default MainContent;
