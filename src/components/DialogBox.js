edcc58b15217af1447372c0b805f2ffd7e7821c7
Meital Lazarovich <lazarovich.meital@gmail.com>
2020-01-06 17:37:29 +0200

    minor changes

--- src/components/DialogBox.js
+++ src/components/DialogBox.js
@@ -2,37 +2,39 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom'
 
 const DialogBox = (props) => {
-    const handleKeydown = (ev) => {
-        if (ev.key === 'Escape') {
-            hideDialog()
-        };
+        const handleKeydown = ({ key }) => {
+            if (key === 'Escape') return hideDialog();
+            // Trapping the tab inside the component without using 'focus-trap' package:
+            console.log('key', key);
+        }
+
+        const hideDialog = () => {
+            props.onClose();
+        }
+
+        useEffect(() => {
+            const contentFocusedEl = document.activeElement
+            const contentEls = document.querySelectorAll('body > div:not(#dialogPlaceholder)')
+            contentEls.forEach(el => el.setAttribute('aria-hidden', true))
+            document.addEventListener("keydown", handleKeydown);
+            return () => {
+                contentFocusedEl.focus()
+                contentEls.forEach(el => el.removeAttribute('aria-hidden'))
+                document.removeEventListener("keydown", handleKeydown);
+            };
+        });
+
+        return ReactDOM.createPortal(
+            <section className={props.className} style={props.style}>
+                <div className="visual-overlay" onClick={hideDialog} ></div>
+                <dialog style={{ display: 'block' }}>
+                    <div role="document">
+                        {props.children}
+                    </div>
+                </dialog>
+            </section>,
+            document.getElementById('dialogPlaceholder')
+        )
     }
 
-    const hideDialog = () => {
-       props.onClose();
-    }
-
-    useEffect(() => {
-        const contentEls = document.querySelectorAll('body > div:not(#dialogPlaceholder)')
-        contentEls.forEach(el => el.setAttribute('aria-hidden', true))
-        document.addEventListener("keydown", handleKeydown);
-        return () => {
-            contentEls.forEach(el => el.setAttribute('aria-hidden', false))
-            document.removeEventListener("keydown", handleKeydown);
-        };
-    });
-
-    return ReactDOM.createPortal(
-        <section className={props.className}>
-            <div className="visual-overlay" onClick={hideDialog} ></div>
-            <dialog style={{ display: 'block' }}>
-                <div role="document">
-                    {props.children}
-                </div>
-            </dialog>
-        </section>,
-        document.getElementById('dialogPlaceholder')
-    )
-}
-
-export default DialogBox;
+    export default DialogBox;
